8f2140fb501e68b1697c039b67056555fc064d22
Tomasz Pakuła <forest10pl@gmail.com>
2024-10-25 14:15:50 +0200

    Use custom version of autoscaling module

--- ec2.tf
+++ ec2.tf
@@ -40,8 +40,7 @@ module "ecs_labels" {
 module "autoscale_group" {
   for_each = local.ec2_capacity_providers
 
-  source  = "cloudposse/ec2-autoscale-group/aws"
-  version = "0.40.0"
+  source = "github.com/Lawstorant/terraform-aws-ec2-autoscale-group"
 
   context = module.ecs_labels[each.key].context
   enabled = true
